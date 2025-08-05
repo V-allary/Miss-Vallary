fcddecefda6d331b91e322fc056837bdb336829e
V-allary <vallarymitchelle4@gmail.com>
2025-08-05 15:20:58 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use (express.statics(__dirname)); // Serve frontend files
-
+app.use(express.static(__dirname)); 
  // Homepage route
  app.get('/', (req,res) =>{
   res.sendFile(__dirname + '/index.html');
